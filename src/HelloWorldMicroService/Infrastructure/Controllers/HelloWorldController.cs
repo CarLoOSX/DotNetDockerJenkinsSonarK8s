d3b0b6df50ef6741400750704fb86a86046ef836
Carlos Martinez <carloosx@MacBook-Pro.local>
2021-03-18 02:11:55 +0100

    base project working

---
+++ src/HelloWorldMicroService/Infrastructure/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using HelloWorldMicroService.Application;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldMicroService.Infrastructure.Controllers
+{
+    [ApiController]
+    [Route("HelloWorld")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+        private readonly HelloWorldApplication _app;
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+            _app = new HelloWorldApplication();
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.Log(LogLevel.Trace, $"@Method: [{nameof(HttpGetAttribute.Name)}] @Controller: {nameof(HelloWorldController)}");
+            return _app.HelloWorldText();
+        }
+    }
+}
